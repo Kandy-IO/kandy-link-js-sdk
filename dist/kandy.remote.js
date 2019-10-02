7d8b977249c8c5d3b83d57ada8a6fc1e98ff6215
machine <janwa039@gmail.com>
2019-10-02 15:30:01 +0000

    148

--- dist/kandy.remote.js
+++ dist/kandy.remote.js
@@ -1,7 +1,7 @@
 /**
  * Kandy.js
  * kandy.remote.js
- * Version: 4.8.0-beta.147
+ * Version: 4.8.0-beta.148
  */
 (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
