e41612ec678cf810b3a6ad2cbdc1c6dee77b588c
machine <janwa039@gmail.com>
2020-04-24 15:32:14 +0000

    384

--- dist/kandy.js
+++ dist/kandy.js
@@ -1,7 +1,7 @@
 /**
  * Kandy.js
  * kandy.newLink.js
- * Version: 4.15.0-beta.383
+ * Version: 4.15.0-beta.384
  */
 (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
@@ -41176,7 +41176,7 @@ exports.getVersion = getVersion;
  * for the @@ tag below with actual version value.
  */
 function getVersion() {
-  return '4.15.0-beta.383';
+  return '4.15.0-beta.384';
 }
 
 /***/ }),
