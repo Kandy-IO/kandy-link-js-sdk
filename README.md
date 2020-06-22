2a3ad90e5c7886a3d99db8aef795cc10a434cd27
machine <janwa039@gmail.com>
2020-06-22 18:47:22 +0000

    451

--- README.md
+++ README.md
@@ -15,7 +15,7 @@ To install a particular build branch or beta branch with the Github URL, run:
 ### In the browser :
 ```html
 <script src="https://unpkg.com/@kandy-io/link-sdk@4.17.0/dist/kandy.js"
-  integrity="sha384-v3/V6Ci5kZe8BbRhJ2p5iYZc+rs10tZZKHLI+UGI2wh1exoIjkVoCGiTBRNqAZay"
+  integrity="sha384-K0SrufQ3khnye/X0eFt2hA3n9o+OfPqCWuZ4qrYpoUlbqiyxNR1O9LqlAqsfX3A3"
   crossorigin="anonymous">
 </script>
 ```
--- dist/kandy.js
+++ dist/kandy.js
@@ -1,7 +1,7 @@
 /**
  * Kandy.js
  * kandy.newLink.js
- * Version: 4.17.0-beta.450
+ * Version: 4.17.0-beta.451
  */
 (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
@@ -28261,6 +28261,7 @@ function callAPI({ dispatch, getState }) {
      * @public
      * @static
      * @memberof call
+     * @requires callMe
      * @requires call
      * @type {Object}
      * @property {string} INITIATING The (outgoing) call is being started. While in this state, no Call operations can be performed until Call gets into Initiated state.
@@ -40842,7 +40843,7 @@ exports.getVersion = getVersion;
  * for the @@ tag below with actual version value.
  */
 function getVersion() {
-  return '4.17.0-beta.450';
+  return '4.17.0-beta.451';
 }
 
 /***/ }),
