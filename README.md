7713e8304ca2b5d66e1d2b1647d590b104af0dee
machine <janwa039@gmail.com>
2022-08-21 11:32:30 +0000

    917

--- README.md
+++ README.md
@@ -15,7 +15,7 @@ To install a particular build branch or beta branch with the Github URL, run:
 ### In the browser :
 ```html
 <script src="https://unpkg.com/@kandy-io/link-sdk@5.1.0/dist/kandy.js"
-  integrity="sha384-5dvryP6KZD8cIoC+N1nVu8EvAvVQUZXFSWrEkyepaIYHTayc25ExGxbLKhrYz9F9"
+  integrity="sha384-LkEiJ9KywZKvi/tDKpCPBkoFuoFo7hr1GS2zu4zsTNtlfeiR8ffPNgBbfokZw7eo"
   crossorigin="anonymous">
 </script>
 ```
--- dist/kandy.remote.js
+++ dist/kandy.remote.js
@@ -1,7 +1,7 @@
 /**
  * Kandy.js
  * kandy.remote.js
- * Version: 5.1.0-beta.916
+ * Version: 5.1.0-beta.917
  */
 (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
@@ -5420,7 +5420,7 @@ exports.getVersion = getVersion;
  * for the @@ tag below with actual version value.
  */
 function getVersion() {
-  return '5.1.0-beta.916';
+  return '5.1.0-beta.917';
 }
 
 /***/ }),
