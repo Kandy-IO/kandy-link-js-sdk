cb5796eb43f9554a921a8cdbc70f7ff7793ca0fe
machine <janwa039@gmail.com>
2022-08-30 12:21:17 +0000

    922

--- dist/kandy.js
+++ dist/kandy.js
@@ -1,7 +1,7 @@
 /**
  * Kandy.js
  * kandy.newLink.js
- * Version: 5.2.0-beta.921
+ * Version: 5.2.0-beta.922
  */
 (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
@@ -6927,7 +6927,7 @@ exports.getVersion = getVersion;
  * for the @@ tag below with actual version value.
  */
 function getVersion() {
-  return '5.2.0-beta.921';
+  return '5.2.0-beta.922';
 }
 
 /***/ }),
@@ -50193,6 +50193,7 @@ function* makeCall(deps, action) {
     const sessionState = yield (0, _effects.select)(_selectors2.getSessionById, sessionId);
 
     yield (0, _effects.put)(_actions.callActions.pendingMakeCall(action.payload.id, {
+      state: _constants.CALL_STATES.INITIATED,
       // The ID that the backend uses to track this webRTC session.
       wrtcsSessionId: response.wrtcsSessionId,
       // The ID that the webRTC stack uses to track this webRTC session.
